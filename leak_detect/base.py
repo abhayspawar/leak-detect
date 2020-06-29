eaf7154caebe5a6e3802e52f6506f57babdeeb06
Abhay Pawar <abhay.pawar@stitchfix.com>
2020-06-28 22:22:54 -0700

    added all the documentation regarding function, inputs and outputs

--- leak_detect/base.py
+++ leak_detect/base.py
@@ -2,6 +2,17 @@ import pandas as pd
 import numpy as np
 
 def get_nan_counts(data, leakage_to_cols, null_col_suffix=''):
+    """
+    Returns dataframe containing number of nulls in 'leakage_to_cols' in 'data'
+
+        Parameters:
+            data (dataframe): input data with 'leakage_to_cols'
+            leakage_to_cols (list): columns used for computing number of nulls in them
+            null_col_suffix (string): suffix used for null_counts column in returned dataframe
+
+        Returns: 
+            nulls_df (dataframe): contains null counts for columns in 'leakage_to_cols' in 'data'.
+    """
     nulls_df = pd.DataFrame(pd.isnull(data[leakage_to_cols]).sum())
     nulls_df.columns = ['null_counts'+null_col_suffix]
     nulls_df['feature'] = nulls_df.index
@@ -9,12 +20,37 @@ def get_nan_counts(data, leakage_to_cols, null_col_suffix=''):
     return nulls_df
 
 def detect_vertical_leakage_from_to(data_creation_func, input_data, input_feature_cols, output_feature_cols, 
-                                      use_nan=True, check_row_number=-1, direction='upward'):    
+                                      use_nan=True, check_row_number=-1, direction='upward'):
+    """
+    Checks if vertical leakage is happening in 'output_feature_cols' columns while creating them from 'input_feature_cols'.
+    Prints out 'output_feature_cols' which have vertically leaked data. Can check for upward (default) or downward leakage.
+    If rows are sorted by date, you would want the data from a row to not flow into previous rows. So, the check should
+    be for 'upward' direction.
+    
+    :param data_creation_func: data creation function which takes in 'input_data' and 
+    returns a dataframe containing 'output_feature_cols' created from 'input_feature_cols'.
+    :param input_data: dataframe containing 'input_feature_cols' and used as input to 'data_creation_func'.
+
+    :param input_feature_cols: columns in input data which are used to compute 'output_feature_cols' in 
+        'data_creation_func'.
+    :param output_feature_cols: columns created in 'data_creation_func' for which we want to check if data has 
+        leaked vertically in given direction.
+    :param use_nan: If NANs should be used. Complex numbers are used is False. Default is NANs.
+    :param check_row_number: row number above or below which all rows are set to NANs/complex numbers depending 
+        on check direction. By default, set
+    to be at the middle in input data.
+    :param direction: Direction in which leakage is to be detected. 'upward' implies 'output_feature_cols' will 
+        be checked for leakage from later 
+    rows into former rows. 'downward' implies in the other direction.
+
+    :return: True if leakage is happening vertically in given direction. Else false.
+    """
+
     input_data_feats = data_creation_func(input_data)
 
-    if len(input_data_feats)!=len(input_data):
-        raise Exception("Number of rows in input data and data created using data_creation_func is different."+
-                        "Do not drop any rows during data creation process.")
+    # if len(input_data_feats)!=len(input_data):
+    #     raise Exception("Number of rows in input data and data created using data_creation_func is different."+
+    #                     "Do not drop any rows during data creation process.")
         
     for col in output_feature_cols:
         if col not in input_data_feats.columns:
@@ -73,7 +109,36 @@ def detect_vertical_leakage_from_to(data_creation_func, input_data, input_featur
     return has_leakage
 
 def detect_vertical_leakage(data_creation_func, input_data, input_feature_cols, output_feature_cols, only_nan=False,
-                           direction='upward', check_row_number=-1):
+                            check_row_number=-1, direction='upward'):
+
+    """
+    Checks if vertical leakage is happening in 'output_feature_cols' columns while creating them from 'input_feature_cols'
+    by using NANs (and complex number if only_nan=False).
+    Prints out 'output_feature_cols' which have vertically leaked data. Can check for upward (default) or downward leakage.
+    If rows are sorted by date, you would want the data from a row to not flow into previous rows. So, the check should
+    be for 'upward' direction.
+    
+        Parameters:
+            data_creation_func (function): data creation function which takes in 'input_data' and 
+                returns a dataframe containing 'output_feature_cols' created from 'input_feature_cols'.
+            input_data (dataframe): containing 'input_feature_cols' and used as input to 'data_creation_func'.
+            input_feature_cols (list): columns in input data which are used to compute 'output_feature_cols' 
+                in 'data_creation_func'.
+            output_feature_cols (list): columns created in 'data_creation_func' for which we want to check 
+                if data has leaked vertically in given direction.
+            only_nan (boolean): If only NANs should be used. By default, complex numbers are also used for 
+                the check separately.
+            check_row_number (int): row number above or below which all rows are set to NANs/complex numbers 
+                depending on check direction. By default, set
+                to be at the middle in input data.
+            direction (str): Direction in which leakage is to be detected. 'upward' implies 'output_feature_cols' 
+                will be checked for leakage from later rows into former rows. 'downward' implies in the other 
+                direction.
+
+        Returns: 
+            has_leakage (boolean): True if leakage is happening vertically in given direction. Else false.
+    """
+
     if direction not in ['upward', 'downward']:
         raise Exception("Please provide 'upward' or 'downward' as a value for 'direction' input")
     for col in input_feature_cols:
@@ -114,13 +179,19 @@ def detect_horizontal_leakage_from_to(data_creation_func, input_data, leakage_fr
     Prints out 'leakage_to_cols' which have leaked data from 'leakage_from_cols' and number of 
     rows with leakage.
     
-    :param data_creation_func: data creation function which takes in 'input_data' and 
-    returns a dataframe containing 'leakage_to_cols' columns with same number of rows as 'input_data'
-    :param input_data: dataframe containing 'leakage_to_cols' and used as input to 'data_creation_func'.
-    :param leakage_from_cols: columns in input data for which we want to check if their data is leaking into 'leakage_to_cols'.
-    :param leakage_to_cols: columns created in 'data_creation_func' for which we want to check if data is leaking from 'leakage_from_cols'.
-    :param use_nan: If NANs should be used. Complex numbers are used is False. Default is NANs.
-    :return: True if leakage is happening from 'leakage_from_cols' to 'leakage_to_cols'.
+        Parameters:
+            data_creation_func (function): data creation function which takes in 'input_data' and 
+                returns a dataframe containing 'leakage_to_cols' columns with same number of rows as 
+                'input_data'
+            input_data (dataframe): containing 'leakage_to_cols' and used as input to 'data_creation_func'.
+            leakage_from_cols (list): columns in input data for which we want to check if their data is 
+                leaking into 'leakage_to_cols'.
+            leakage_to_cols (list): columns created in 'data_creation_func' for which we want to check if 
+                data is leaking from 'leakage_from_cols'.
+            use_nan (boolean): If NANs should be used. Complex numbers are used is False. Default is NANs.
+
+        Returns:
+            has_leakage (boolean): True if leakage is happening from 'leakage_from_cols' to 'leakage_to_cols'.
     """
     
     # checks for leakage from 'from' cols to 'to' cols. Gets called by main horizontal leakage function.
@@ -165,23 +236,27 @@ def detect_horizontal_leakage_from_to(data_creation_func, input_data, leakage_fr
 
 def detect_horizontal_leakage(data_creation_func, input_data, target_cols, output_feature_cols, input_feature_cols=[], 
                               only_nan=False):
-
     """
     Checks if leakage is happening from 'target_cols' columns to 'output_feature_cols' columns.
     Prints out 'output_feature_cols' which have leaked data from 'target_cols' and number of 
     rows with leakage.
     If 'input_feature_cols' are passed, then also checks leakage from these to 'target_cols'.
 
-    :param data_creation_func: data creation function which takes in 'input_data' and 
-    returns a dataframe containing 'leakage_to_cols' columns with same number of rows as 'input_data'
-    :param input_data: dataframe containing pre-computed 'target_cols'. If 'input_feature_cols' are passed, they
-    are required to be there in 'input_data'.
-    :param target_cols: List of columns containg dependent variables.
-    :param output_feature_cols: columns created in 'data_creation_func' for which we want to check if data is leaking from 'target_cols'.
-    :param input_feature_cols: columns created in 'input_data' for which we want to check if data is leaking into 'target_cols'.
-    By deafult, no columns are passed and this test is not done.
-    :param only_nan: If NANs should be used. Complex numbers are used is False. Default is NANs.
-    :return: True if leakage is happening from 'leakage_from_cols' to 'leakage_to_cols'.
+        Parameters:
+            data_creation_func (function): data creation function which takes in 'input_data' and 
+                returns a dataframe containing 'leakage_to_cols' columns with same number of rows as 'input_data'
+            input_data (dataframe): containing pre-computed 'target_cols'. If 'input_feature_cols' are passed, they
+                are required to be there in 'input_data'.
+            target_cols (list): List of columns containg dependent variables.
+            output_feature_cols (list): columns created in 'data_creation_func' for which we want to check if data 
+                is leaking from 'target_cols'.
+            input_feature_cols (list): columns from 'input_date' used in 'data_creation_func' to create features data. 
+                We want to check if data is leaking from these columns into 'target_cols'. 
+                By deafult, no columns are passed and this test is not done.
+            only_nan (boolean): If NANs should be used. Complex numbers are used is False. Default is NANs.
+
+        Returns:
+            has_leakage (boolean): True if leakage is happening from 'leakage_from_cols' to 'leakage_to_cols'.
     """
 
     # Main function for horizontal leakage. Checks for two leakage from target cols to feature cols and vice versa 
